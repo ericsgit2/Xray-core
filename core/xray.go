2f6596128f2654ed006efb3b9d41cfffe8441e5e
Eric Lee <maildroid9999@gmail.com>
2026-01-16 02:41:44 +0800

    Update xray.go

--- core/xray.go
+++ core/xray.go
@@ -392,7 +392,7 @@ func (s *Instance) Start() error {
 		}
 	}
 
-	errors.LogWarning(s.ctx, "Xray ", Version(), " started")
+	errors.LogWarning(s.ctx, "server ", Version(), " started")
 
 	return nil
 }
